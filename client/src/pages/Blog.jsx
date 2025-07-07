91d4bdfd70e5e593af4f5bac2aa1c4bad96c546d
unknown <amanverma9748@gmail.com>
2025-07-07 20:55:45 +0530

    BGC to Blog page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { blog_data } from '../assets/assets'
+import { assets, blog_data } from '../assets/assets'
+import Navbar from '../components/Navbar'
 
 const Blog = () => {
 
@@ -18,8 +19,14 @@ const Blog = () => {
   }, [])
 
   return data ? (
-    <div>
-      <h1>Blog</h1>
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
+      <Navbar />
+
+      <div></div>
+
+      <div></div>
+
     </div>
   ) : <div>Loading...</div>
 }
