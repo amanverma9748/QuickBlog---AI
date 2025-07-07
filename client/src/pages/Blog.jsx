a7ba0a78025f73db5fa4593b86fa6f6c546f4552
unknown <amanverma9748@gmail.com>
2025-07-07 20:46:24 +0530

    Project Day 2

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { blog_data } from '../assets/assets'
 
 const Blog = () => {
-  return (
+
+  const { id } = useParams()
+
+  const [data, setdata] = useState(null)
+
+  const fetchBlogData = async () => {
+    const data = blog_data.find(item => item._id === id)
+    setdata(data)
+  }
+
+  useEffect(() => {
+    fetchBlogData()
+  }, [])
+
+  return data ? (
     <div>
       <h1>Blog</h1>
     </div>
-  )
+  ) : <div>Loading...</div>
 }
 
 export default Blog
