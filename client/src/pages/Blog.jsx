bd31d52558f0e329c002f84b5731b75a0fc04209
unknown <amanverma9748@gmail.com>
2025-07-08 19:10:04 +0530

    Completed blog detail page by adding footer

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Moment from 'moment'
+import Footer from '../components/Footer'
 
 const Blog = () => {
 
@@ -74,8 +75,21 @@ const Blog = () => {
           </form>
 
         </div>
+        {/*Share Buttons */}
+        <div className='my-24 max-w-3xl mx-auto'>
+
+
+          <p className='font-semibold my-4'>Share this article on social media</p>
+
+          <div className='flex'>
+            <img src={assets.facebook_icon} width={50} alt="" />
+            <img src={assets.twitter_icon} width={50} alt="" />
+            <img src={assets.googleplus_icon} width={50} alt="" />
+          </div>
+        </div>
 
       </div>
+      <Footer />
 
     </div>
   ) : <div>Loading...</div>
